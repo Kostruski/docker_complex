a1f39cb89fbf1c98192413f8b65a0473a73d0cbc
Marek-Kostruski <mkkos123@gmail.com>
2020-04-05 08:29:40 +0200

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div); app wysyła zapytanie do bazy któa nie jest jescze uruchomiona 
+});
